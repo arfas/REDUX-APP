960fd11e0b442bb5d1eb4b098be3dd6e354bd4b5
Arfa <arfas@ymail.com>
2019-03-12 00:24:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './componenets/Posts';
+import PostForm from './componenets/PostForm';
 
+import {Provider} from 'react-redux';
+import store from './store';
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1 className="App-title">Welcome to react</h1>
+          </header>
+          <PostForm/>
+          <Posts />
       </div>
+      </Provider>
     );
   }
 }
